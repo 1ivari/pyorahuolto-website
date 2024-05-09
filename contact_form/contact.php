b02ce09510e83329bb276ca9a3096c92073298ac
1ivari <60034643+1ivari@users.noreply.github.com>
2024-05-09 11:11:00 +0300

    php update

--- contact_form/contact.php
+++ contact_form/contact.php
@@ -15,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $headers = "From:" . $from;
   if (mail(implode(",", $to), $subject, $body, $headers)) {
     // header("Location: /index.html");  // Redirect to the home page
+    echo "Lähetetty.";
     exit();
   } else {
     echo "Sorry, there was an error sending your message. Please try again later.";
